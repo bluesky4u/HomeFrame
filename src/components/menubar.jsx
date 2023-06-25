c30f84920bf1a2c238e1bd1b4a886567015821dd
bluesky4u <n.melo1013@gmail.com>
2023-06-25 02:37:25 -0400

    fix: main_slider

--- src/components/menubar.jsx
+++ src/components/menubar.jsx
@@ -10,9 +10,8 @@ const MenuBar = () => {
             <nav className="flex justify-between text-white w-screen">
                 <div className="px-5 xl:px-12 py-6 flex w-full items-center">
                     <a className="text-3xl font-bold font-heading" href="#">
-                        <StylizeoIcon />
+                        <StylizeoIcon className="h-9"/>
                         {/* <!-- <img className="h-9" src="logo.png" alt="logo"> --> */}
-                        Logo Here.
                     </a>
                     {/* <!-- Nav Links --> */}
                     <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
--- src/views/test/index.jsx
+++ src/views/test/index.jsx
@@ -2,20 +2,46 @@ import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/react-splide/css';
 
 const Slider = () => {
-  return (
-    <div className='main'>
-        <Splide options={ { rewind: true } } aria-label="React Splide Example">
-            <SplideSlide>
-                <img src="/assets/img/1.png" alt="1"/>
-            </SplideSlide>
-            <SplideSlide>
-                <img src="/assets/img/2.jpeg" alt="2"/>
-            </SplideSlide>
-            <SplideSlide>
-                <img src="/assets/img/3.png" alt="2"/>
-            </SplideSlide>
-        </Splide>
-    </div>
-  );
+    return (
+        <div className='main'>
+            <Splide options={{
+                rewind: 'true',
+                type: 'loop',
+                perPage: 3,
+                focus: 'center', gap: 40
+            }} aria-label="React Splide Example">
+                <SplideSlide>
+                    <div className='component'>
+                        <div className='frame'>
+                            <img src='/assets/img/colorDefault.jpeg' />
+                        </div>
+                        <div className='image'>
+                            <img src='/assets/img/1.png' />
+                        </div>
+                    </div>
+                </SplideSlide>
+                <SplideSlide>
+                    <div className='component'>
+                        <div className='frame'>
+                            <img src='/assets/img/colorDefault.jpeg' />
+                        </div>
+                        <div className='image'>
+                            <img src='/assets/img/2.png' />
+                        </div>
+                    </div>
+                </SplideSlide>
+                <SplideSlide>
+                    <div className='component'>
+                        <div className='frame'>
+                            <img src='/assets/img/colorDefault.jpeg' />
+                        </div>
+                        <div className='image'>
+                            <img src='/assets/img/3.png' />
+                        </div>
+                    </div>
+                </SplideSlide>
+            </Splide>
+        </div>
+    );
 }
 export default Slider;
