7e992f02ab9f39e308f082b2ea31e68dd4343070
bluesky4u <n.melo1013@gmail.com>
2023-06-25 00:33:11 -0400

    fix: header footer

--- src/views/test/index.jsx
+++ src/views/test/index.jsx
@@ -1,9 +1,21 @@
-const TestView = () => {
-    return (
-        <>
-            <div className="relative">TEst</div>
-        </>
-    )
-}
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
 
-export default TestView;
+const Slider = () => {
+  return (
+    <div className='main'>
+        <Splide options={ { rewind: true } } aria-label="React Splide Example">
+            <SplideSlide>
+                <img src="/assets/img/1.png" alt="1"/>
+            </SplideSlide>
+            <SplideSlide>
+                <img src="/assets/img/2.jpeg" alt="2"/>
+            </SplideSlide>
+            <SplideSlide>
+                <img src="/assets/img/3.png" alt="2"/>
+            </SplideSlide>
+        </Splide>
+    </div>
+  );
+}
+export default Slider;
